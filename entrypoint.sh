d8c7a13cff70f3678860ba4b4621f5b554e8e7d3
Reiner Saddey <Reiner.Saddey@carano.de>
2016-11-09 20:23:31 +0000

    Full HD screen 1920x1080x24 instead of 1280x780x24

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,9 @@
 # start something
 # TODO experimentally hacked
 # 26-aug-16 1280x720x24 instead of 1280x780x24
+# 09-nov-16 Full HD screen 1920x1080x24 instead of 1280x780x24
 
-rm -f /tmp/.X99-lock && /usr/bin/Xvfb :99 -screen 0 1280x780x24 -ac &
+rm -f /tmp/.X99-lock && /usr/bin/Xvfb :99 -screen 0 1920x1080x24 -ac &
 x11vnc -q -shared -display WAIT:99 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900 &
 
